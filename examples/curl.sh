919f01e1a759a6cd392e144231e055dadb745ee1
Ed Sweeney <ed@onextent.com>
2019-05-26 21:22:20 -0700

    example

--- examples/curl.sh
+++ examples/curl.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -d '[{"sourceId": "b2546ee7-494f-42cc-abe0-9113c3eefee2"},{"sourceId": "99999999-494f-42cc-abe0-9113c3eefee2"}]' -H "Content-Type: application/json" -X POST https://lotsofnames.navicore.tech/names
+#curl -d '[{"sourceId": "b2546ee7-494f-42cc-abe0-9113c3eefee2"},{"sourceId": "99999999-494f-42cc-abe0-9113c3eefee2"}]' -H "Content-Type: application/json" -X POST https://lotsofnames.navicore.tech/names
+
+curl -d '[{"sourceId": "7AC7E2AD-50E2-4515-8392-A83B38CE9144"}]' -H "Content-Type: application/json" -X POST https://lotsofnames.navicore.tech/names
